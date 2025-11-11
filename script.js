e11cb79b72feb5bbef097d34724bbfa6ff781262
Noura <haziqarie3@gmail.com>
2025-11-11 10:24:30 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e){
-  e.preventDefault();
-  document.getElementById('formResult').textContent = 'Message received!';
-  this.reset();
-});
